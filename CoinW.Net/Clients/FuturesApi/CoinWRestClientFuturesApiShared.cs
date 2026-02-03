420be2a8858b9ec0829bb22ad8c9b76734655f6a
JKorf <jankorf91@gmail.com>
2026-02-03 20:36:06 +0100

    wip

--- CoinW.Net/Clients/FuturesApi/CoinWRestClientFuturesApiShared.cs
+++ CoinW.Net/Clients/FuturesApi/CoinWRestClientFuturesApiShared.cs
@@ -195,7 +195,7 @@ namespace CoinW.Net.Clients.FuturesApi
             return resultData;
         }
 
-        public async Task<ExchangeResult<SharedSymbol[]>> GetFuturesSymbolsForBaseAssetAsync(string baseAsset)
+        async Task<ExchangeResult<SharedSymbol[]>> IFuturesSymbolRestClient.GetFuturesSymbolsForBaseAssetAsync(string baseAsset)
         {
             if (!ExchangeSymbolCache.HasCached(_topicId))
             {
@@ -207,7 +207,7 @@ namespace CoinW.Net.Clients.FuturesApi
             return new ExchangeResult<SharedSymbol[]>(Exchange, ExchangeSymbolCache.GetSymbolsForBaseAsset(_topicId, baseAsset));
         }
 
-        public async Task<ExchangeResult<bool>> SupportsFuturesSymbolAsync(SharedSymbol symbol)
+        async Task<ExchangeResult<bool>> IFuturesSymbolRestClient.SupportsFuturesSymbolAsync(SharedSymbol symbol)
         {
             if (symbol.TradingMode == TradingMode.Spot)
                 throw new ArgumentException(nameof(symbol), "Spot symbols not allowed");
@@ -222,7 +222,7 @@ namespace CoinW.Net.Clients.FuturesApi
             return new ExchangeResult<bool>(Exchange, ExchangeSymbolCache.SupportsSymbol(_topicId, symbol));
         }
 
-        public async Task<ExchangeResult<bool>> SupportsFuturesSymbolAsync(string symbolName)
+        async Task<ExchangeResult<bool>> IFuturesSymbolRestClient.SupportsFuturesSymbolAsync(string symbolName)
         {
             if (!ExchangeSymbolCache.HasCached(_topicId))
             {
