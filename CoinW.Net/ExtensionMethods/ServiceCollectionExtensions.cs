311d14db7fdde42e4860493e80ee8f2145bcff5f
Jkorf <jankorf91@gmail.com>
2026-02-24 13:17:31 +0100

    Updated HttpClient registration logic

--- CoinW.Net/ExtensionMethods/ServiceCollectionExtensions.cs
+++ CoinW.Net/ExtensionMethods/ServiceCollectionExtensions.cs
@@ -13,6 +13,7 @@ using CoinW.Net.Interfaces.Clients;
 using CoinW.Net.Objects.Options;
 using CoinW.Net.SymbolOrderBooks;
 using CryptoExchange.Net.Interfaces.Clients;
+using System.Threading;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -95,8 +96,8 @@ namespace Microsoft.Extensions.DependencyInjection
                 return new CoinWRestClient(client, serviceProvider.GetRequiredService<ILoggerFactory>(), serviceProvider.GetRequiredService<IOptions<CoinWRestOptions>>());
             }).ConfigurePrimaryHttpMessageHandler((serviceProvider) => {
                 var options = serviceProvider.GetRequiredService<IOptions<CoinWRestOptions>>().Value;
-                return LibraryHelpers.CreateHttpClientMessageHandler(options.Proxy, options.HttpKeepAliveInterval);
-            });
+                return LibraryHelpers.CreateHttpClientMessageHandler(options);
+            }).SetHandlerLifetime(Timeout.InfiniteTimeSpan);
             services.Add(new ServiceDescriptor(typeof(ICoinWSocketClient), x => { return new CoinWSocketClient(x.GetRequiredService<IOptions<CoinWSocketOptions>>(), x.GetRequiredService<ILoggerFactory>()); }, socketClientLifeTime ?? ServiceLifetime.Singleton));
 
             services.AddTransient<ICryptoRestClient, CryptoRestClient>();
@@ -106,7 +107,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddTransient<ITrackerFactory, CoinWTrackerFactory>();
             services.AddSingleton<ICoinWUserClientProvider, CoinWUserClientProvider>(x =>
                 new CoinWUserClientProvider(
-                    x.GetRequiredService<HttpClient>(),
+                x.GetRequiredService<IHttpClientFactory>().CreateClient(typeof(ICoinWRestClient).Name),
                     x.GetRequiredService<ILoggerFactory>(),
                     x.GetRequiredService<IOptions<CoinWRestOptions>>(),
                     x.GetRequiredService<IOptions<CoinWSocketOptions>>()));
